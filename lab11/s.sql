e5552fa4f03b33edd27fb692b3ce5644517b6d8c
bart0003 <103779846+bart0003@users.noreply.github.com>
2022-04-23 18:56:18 +0300

    delete

--- lab11/s.sql
+++
@@ -1,3 +0,0 @@
-start transaction isolation level read uncommitted;
-UPDATE isolation SET username = 'ajones' WHERE username = 'jones';
-UPDATE isolation SET balance = balance + 10 WHERE username = 'ajones';
